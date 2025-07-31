5b88143db00ce8a9a1c9299bbcc5b4c15c535e80
Billy.Zheng <vil963@gmail.com>
2025-07-31 21:23:59 +0800

    Added spec

--- spec/flows/reply_spec.cr
+++ spec/flows/reply_spec.cr
@@ -5,5 +5,10 @@ describe "Create reply to doc and reply to reply", tags: "flow" do
     flow = AuthenticationFlow.new("test@example.com")
     flow.sign_up "password"
     flow.should_be_signed_in
+    flow.open_doc
+    flow.create_two_reply_to_doc
+    flow.delete_first_reply
+    flow.edit_reply
+    flow.should have_element(css_selector: "article#doc_reply-2", text: "hello world!")
   end
 end
