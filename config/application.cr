2000b210e63cd64d8c6aec9498df8a8e72fa6a6b
Billy.Zheng <vil963@gmail.com>
2025-11-02 15:55:41 +0800

    Fix update error

--- config/application.cr
+++ config/application.cr
@@ -30,19 +30,19 @@ end
 
 # Cache 默认是开启压缩的。
 CACHE_STORE = if LuckyEnv.production?
-                Cache::MemoryStore(String, String)
+                Cache::MemoryStore(String)
               else
-                Cache::NullStore(String, String)
+                Cache::NullStore(String)
               end
 
-CAPTCHA_CACHE       = Cache::MemoryStore(String, String).new(expires_in: 1.minute)
+CAPTCHA_CACHE       = Cache::MemoryStore(String).new(expires_in: 1.minute)
 MARKDOWN_CACHE      = CACHE_STORE.new(expires_in: 1.day)
-ONLINE_USER_COUNTER = Cache::MemoryStore(String, Int64).new(expires_in: 3.minutes)
+ONLINE_USER_COUNTER = Cache::MemoryStore(Int64).new(expires_in: 3.minutes)
 COUNTER_MUTEX       = Mutex.new
 
 ONLINE_IP_COUNTER = CACHE_STORE.new(expires_in: 3.minutes)
 
-VIEW_COUNT_CACHE = Cache::MemoryStore(String, Bool).new(expires_in: 30.minutes)
+VIEW_COUNT_CACHE = Cache::MemoryStore(Bool).new(expires_in: 30.minutes)
 
 # upload image into freeimage.host API key.
 FREEIMAGE_HOST_API_KEY = ENV["FREEIMAGE_HOST_API_KEY"]? || "fake_key"
