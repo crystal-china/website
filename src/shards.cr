78aaff6715c43fbe64e22511a82149685b292549
Billy.Zheng <vil963@gmail.com>
2026-01-10 21:51:44 +0800

    Use captcha 0.2.0 instead of simple_captcha

--- src/shards.cr
+++ src/shards.cr
@@ -16,7 +16,7 @@ require "cron_scheduler"
 require "time_in_words"
 # require "lucky_cache"
 require "cache"
-require "simple_captcha"
+require "captcha"
 require "multi_auth"
 MultiAuth.config("github", ENV["GITHUB_OAUTH_CLIENT_ID"]? || "", ENV["GITHUB_OAUTH_SECRET"]? || "")
 MultiAuth.config("google", ENV["GOOGLE_OAUTH_CLIENT_ID"]? || "", ENV["GOOGLE_OAUTH_SECRET"]? || "")
