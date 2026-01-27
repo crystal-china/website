476a5fe699a57e8ca191494c1c9cef323300b75d
Billy.Zheng <vil963@gmail.com>
2026-01-27 20:34:37 +0800

    Revert 1070a60

--- src/actions/me/update.cr
+++ src/actions/me/update.cr
@@ -5,7 +5,6 @@ class Me::Update < BrowserAction
     UpdateUser.update(me, params) do |op, _user|
       if op.saved?
         flash.success = "更新成功！"
-        # redirect_back fallback: Home::Index
         redirect Home::Index
       else
         build_failed_flash(op)
