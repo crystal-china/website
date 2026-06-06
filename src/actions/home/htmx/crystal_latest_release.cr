1f17cf92f61d3d6e3b0afe00c0aae2ebdb879913
Billy.Zheng <vil963@gmail.com>
2026-06-06 17:53:18 +0800

    develpment not visit crystal-lang.org to get the version

--- src/actions/home/htmx/crystal_latest_release.cr
+++ src/actions/home/htmx/crystal_latest_release.cr
@@ -1,20 +1,28 @@
 class Home::Htmx::CrystalLatestRelease < BrowserAction
   include Auth::AllowGuests
 
-  get "/htmx/crystal_latest_release" do
-    result = <<-'HEREDOC'
+  DEFAULT_RESULT = <<-'HEREDOC'
 <div class="latest-release-info">
-      <a href="https://crystal-lang.org/2025/05/12/1.16.3-released/">Latest release: <strong>1.16.3</strong></a>"
+      <a href="https://crystal-lang.org/2025/05/12/1.16.3-released/">Latest release: <strong>1.16.3</strong></a>
 </div>
 HEREDOC
 
-    res = HTTP::Client.get("https://crystal-lang.org")
+  get "/htmx/crystal_latest_release" do
+    result = DEFAULT_RESULT
 
-    if res.success?
-      match_data = res.body.match(%r{<div class="latest-release-info">.+?</div>}m)
-      if !match_data.nil?
-        result = match_data.to_s.sub(/href="(.+)?"/, "href=\"https://crystal-lang.org\\1\"")
+    return plain_text result if LuckyEnv.development?
+
+    begin
+      res = HTTP::Client.get("https://crystal-lang.org")
+
+      if res.success?
+        match_data = res.body.match(%r{<div class="latest-release-info">.+?</div>}m)
+        if !match_data.nil?
+          result = match_data.to_s.sub(/href="(.+)?"/, "href=\"https://crystal-lang.org\\1\"")
+        end
       end
+    rescue Socket::ConnectError | IO::TimeoutError
+      # Fall back to the pinned release snippet when crystal-lang.org is unavailable.
     end
 
     plain_text result
