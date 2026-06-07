9307a7e2ac8f4468b10f277d03a114192022f528
Billy.Zheng <vil963@gmail.com>
2026-06-07 10:44:00 +0800

    use DevAssetCacheHandler to prevent cache in development

--- src/app_server.cr
+++ src/app_server.cr
@@ -10,6 +10,7 @@ class AppServer < Lucky::BaseAppServer
       Lucky::ErrorHandler.new(action: Errors::Show),
       Lucky::RemoteIpHandler.new,
       Lucky::RouteHandler.new,
+      Lucky::DevAssetCacheHandler.new(enabled: LuckyEnv.development?),
       LuckyEnv.production? ? Lucky::StaticCompressionHandler.new("./public", file_ext: "br", content_encoding: "br") : nil,
       LuckyEnv.production? ? Lucky::StaticCompressionHandler.new("./public", file_ext: "gz", content_encoding: "gzip") : nil,
       Lucky::StaticFileHandler.new("./public", fallthrough: false, directory_listing: false),
