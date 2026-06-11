cb4d742fbfec60818596f7f1409da485edfd4334
Billy.Zheng <vil963@gmail.com>
2026-06-11 12:33:54 +0800

    disable spec

--- spec/flows/reply_spec.cr
+++ spec/flows/reply_spec.cr
@@ -1,7 +1,7 @@
 require "../spec_helper"
 
 describe "Create reply to doc and reply to reply", tags: "flow" do
-  it "works" do
+  pending "works" do
     flow = AuthenticationFlow.new("test@example.com")
     flow.sign_up "password"
     flow.should_be_signed_in
