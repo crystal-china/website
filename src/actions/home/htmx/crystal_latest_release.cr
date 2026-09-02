33c929f98dec27c50dcbd9b9a151d5c6ecbfe93d
Billy.Zheng <vil963@gmail.com>
2026-09-02 22:23:08 +0800

    Added: release cache

--- src/actions/home/htmx/crystal_latest_release.cr
+++ src/actions/home/htmx/crystal_latest_release.cr
@@ -1,33 +1,39 @@
 class Home::Htmx::CrystalLatestRelease < BrowserAction
   include Auth::AllowGuests
 
-  UNAVAILABLE_RESULT = <<-'HEREDOC'
-<div class="latest-release-info">
-      <a href="https://crystal-lang.org/">Latest release: <strong>unknown</strong></a>
-</div>
-HEREDOC
+  LATEST_RELEASE_CACHE_KEY = "latest_version"
+  UNKNOWN_VERSION          = "unknown"
 
   get "/htmx/crystal_latest_release" do
-    plain_text LATEST_RELEASE_CACHE.fetch("current") { latest_release_result }
+    version = LATEST_RELEASE_CACHE.read(LATEST_RELEASE_CACHE_KEY) || latest_release_version
+    plain_text <<-HTML
+      <div class="latest-release-info">
+        <a href="https://crystal-lang.org/">Latest release: <strong>#{version}</strong></a>
+      </div>
+    HTML
   end
 
-  private def latest_release_result
-    return UNAVAILABLE_RESULT if LuckyEnv.development?
+  private def latest_release_version
+    return UNKNOWN_VERSION if LuckyEnv.development?
 
     HTTP::Client.new("crystal-lang.org", tls: true) do |client|
-      client.connect_timeout = 3.seconds
-      client.read_timeout = 5.seconds
+      # client.connect_timeout = 3.seconds
+      # client.read_timeout = 5.seconds
       res = client.get("/")
 
       if res.success?
-        match_data = res.body.match(%r{<div class="latest-release-info">.+?</div>}m)
-        return match_data.to_s.sub(/href="(.+)?"/, "href=\"https://crystal-lang.org\\1\"") if match_data
+        if (match_data = res.body.match(%r{<div class="latest-release-info">(.+?)</div>}m))
+          if (release = match_data[1]?)
+            if version = release[/Latest release:\D*([0-9.]+)/, 1]?
+              return LATEST_RELEASE_CACHE.write(LATEST_RELEASE_CACHE_KEY, version)
+            end
+          end
+        end
       end
     end
 
-    UNAVAILABLE_RESULT
+    UNKNOWN_VERSION
   rescue Socket::ConnectError | IO::TimeoutError
-    # Report the unavailable state instead of returning a potentially stale version.
-    UNAVAILABLE_RESULT
+    UNKNOWN_VERSION
   end
 end
