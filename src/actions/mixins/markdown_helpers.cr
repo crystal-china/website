aef4b893e6f19d9d3062ca91f5e6218247fddc4e
Billy.Zheng <vil963@gmail.com>
2026-09-18 09:13:59 +0800

    Fix tab scheduler cache

--- src/actions/mixins/markdown_helpers.cr
+++ src/actions/mixins/markdown_helpers.cr
@@ -1,7 +1,9 @@
 require "../../../tasks/db/seed/hourly_availability"
 
 module MarkdownHelpers
-  FRONT_MATTER_RE = /\A---[ \t]*\n(?<yaml>.*?)\n---[ \t]*\n?/m
+  FRONT_MATTER_RE             = /\A---[ \t]*\n(?<yaml>.*?)\n---[ \t]*\n?/m
+  TABLE_SCHEDULER_RE          = /TableScheduler20250703 year: (\d+), month: (\d+)/
+  TABLE_SCHEDULER_PLACEHOLDER = %(<div data-table-scheduler></div>)
 
   def markdown_path : String
     request_path = current_path.sub(%r{\A/docs/}, "")
@@ -21,23 +23,29 @@ module MarkdownHelpers
 
   def content
     content = markdown_body
+    scheduler = content.match(TABLE_SCHEDULER_RE).try do |match|
+      {match[1].to_i, match[2].to_i}
+    end
 
-    regex = /TableScheduler20250703 year: (\d+), month: (\d+)/
-
-    if content.match(regex)
-      year = $1.to_i
-      month = $2.to_i
+    if scheduler
+      year, month = scheduler
 
       Db::Seed::HourlyAvailabilityTask.run(year, month) if HourlyAvailabilityQuery.new.date("#{year}-#{month}-01").none?
 
-      content = content.sub(
-        regex,
+      content = content.sub(TABLE_SCHEDULER_RE) { TABLE_SCHEDULER_PLACEHOLDER }
+    end
+
+    html = MARKDOWN_CACHE.fetch(markdown_path) { markdown content }
+
+    if scheduler
+      year, month = scheduler
+      html = html.sub(TABLE_SCHEDULER_PLACEHOLDER) do
         TableScheduler.new(year: year, month: month, current_user: current_user).render_to_string
-      )
+      end
     end
 
     div class: "prose" do
-      raw(MARKDOWN_CACHE.fetch(markdown_path) { markdown content })
+      raw html
     end
   end
 
