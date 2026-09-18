17498f740b58dd69ada08a842d84ee6b871f5604
Billy.Zheng <vil963@gmail.com>
2026-09-19 00:39:45 +0800

    delete comment add lock

--- src/actions/htmx/comments/delete.cr
+++ src/actions/htmx/comments/delete.cr
@@ -3,13 +3,17 @@ class Htmx::Comments::Delete < CommentAction
     me = current_user
     return head 401 if me.nil?
 
-    comment = CommentQuery.find(id)
+    status = 200
 
-    return head 403 if comment.user_id != me.id
-    return head 409 if comment.children_count > 0
+    AppDatabase.transaction do
+      comment = CommentQuery.new.id(id).for_update.first
 
-    DeleteComment.delete!(comment)
+      next status = 403 if comment.user_id != me.id
+      next status = 409 if comment.children_count > 0
 
-    head 200
+      DeleteComment.delete!(comment)
+    end
+
+    head status
   end
 end
