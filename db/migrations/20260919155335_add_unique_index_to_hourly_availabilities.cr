5d29c12aa296b22e9bdb1d28323adf96afbfdcd7
Billy.Zheng <vil963@gmail.com>
2026-09-20 11:21:08 +0800

    Squash migration

--- db/migrations/20260919155335_add_unique_index_to_hourly_availabilities.cr
+++
@@ -1,9 +0,0 @@
-class AddUniqueIndexToHourlyAvailabilities::V20260919155335 < Avram::Migrator::Migration::V1
-  def migrate
-    create_index table_for(HourlyAvailability), [:date, :hour], unique: true
-  end
-
-  def rollback
-    drop_index table_for(HourlyAvailability), [:date, :hour]
-  end
-end
