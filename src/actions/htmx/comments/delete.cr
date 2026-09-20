784358efbef675a0e45b98197c400e90ab3fb91d
Billy.Zheng <vil963@gmail.com>
2026-09-20 09:16:41 +0800

    use hx-partial replace empty response replace

--- src/actions/htmx/comments/delete.cr
+++ src/actions/htmx/comments/delete.cr
@@ -29,7 +29,19 @@ class Htmx::Comments::Delete < CommentAction
 
     return head status unless status == 200
 
+    # 这里改为双 partial 方案。
+    # 之前的一个 partial，另一个空响应方尝试删除卡片，案有个坑：
+    
+    # 1. 提取并处理 hx-partial。
+    # 2. 发现剩余的主响应为空。
+    # 3. 将其判断为“只处理 partial”，默认不再交换主 target。
+    # 4. 必须 swapEmpty:true 明确要求它继续执行空的主交换，从而删除评论卡片。
     plain_text <<-HTML
+      <hx-partial
+        hx-target="#comment-#{id}"
+        hx-swap="delete swap:1s"
+      ></hx-partial>
+
       <hx-partial
         hx-target="##{comments_list_id}-count"
       >
