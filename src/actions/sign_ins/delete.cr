d0efb13819d0436bf9c2509917bb6673b7ce3d6a
Billy.Zheng <vil963@gmail.com>
2026-09-25 15:58:06 +0800

    HTMX 收到 HX-Redirect 后执行真正的浏览器页面跳转，而不是只替换 body。新   登录页的 head、Session Cookie 和 CSRF meta 会一起重新加载，因此后续 PATCH 使用的新 token   与 Session 一致，不再出现 403

--- src/actions/sign_ins/delete.cr
+++ src/actions/sign_ins/delete.cr
@@ -2,6 +2,12 @@ class SignIns::Delete < BrowserAction
   delete "/sign_out" do
     sign_out
     flash.success = "取消登录成功"
-    redirect to: SignIns::New
+
+    if context.request.headers["HX-Request"]?
+      context.response.headers["HX-Redirect"] = SignIns::New.path
+      head 200
+    else
+      redirect to: SignIns::New
+    end
   end
 end
